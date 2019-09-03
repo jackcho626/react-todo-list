e8c0f2917323f726da70a00c22337930f5922471
choj <jack.cho@moesol.com>
2019-09-03 15:01:36 -0700

    Set up state, testing GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React To-do List
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <CreateTask />
     </div>
+  );
+}
+
+function CreateTask() {
+  return (
+    <h1>Create Task:</h1>
+
   );
 }
 
