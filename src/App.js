572a1ec6e05615bd641f5a4f6d30f125c7b77129
choj <jack.cho@moesol.com>
2019-09-04 15:31:10 -0700

    List shown, containerization, bugfix, minor style fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import { create } from 'domain';
 
-function App() {
-  const [todoList, setTodoList] = useState([]);
+const Todo = ({ todo }) => (
+  <div>
+    { todo.task }
+  </div>
+)
 
-  return (
-    <div className="App">
-      <CreateTodo />
-      <header className="App-header">
-        <p>
-          React To-do List
-        </p>
-      </header>
+const CreateTodo = ({ todo, setTodo, createTodo }) => (
+  <div> 
+    <h2>Create To-do: </h2>
+    <form onSubmit={ () => createTodo(todo) }> 
+      <input onChange={ e => setTodo(e.target.value) } />
+    </form>
+  </div>
+)
+const CreateTodoContainer = ({ todoList, setTodoList }) => {
+  const [todo, setTodo] = useState('')
+
+  const createTodo = task => {
+    const newList = [ ...todoList, { task } ]
+    setTodoList(newList)
+  }
+
+  return <CreateTodo todo={ todo } setTodo={ setTodo } createTodo={ createTodo } />
+}
+   
+const App = ({ todoList, setTodoList }) => (
+  <div className='App'>
+    <CreateTodoContainer todoList={ todoList } setTodoList={ setTodoList } />
+    <header className='App-header'>
+      <p>
+        React To-do List
+      </p>
+    </header>
+    <div>
+      {
+        todoList.map(todo => <Todo todo={ todo } />)
+      }
     </div>
-  );
+  </div>
+)
+const AppContainer = () => {
+  const [todoList, setTodoList] = useState([{ task: 'Do laundry'}, { task: 'Take out trash'}])
+
+  return <App todoList={ todoList } setTodoList={ setTodoList } />
 }
 
-function CreateTodo({createTodo}) {
-  const [todo, setTodo] = useState("");
-  return (
-    <div> 
-      <h2>Create To-do: </h2>
-      <form>
-        <input onChange={ e => setTodo(e.target.value) } />
-      </form>
-    </div>
-  );
-}
-
-export default App;
+export default AppContainer
