ca20395ef84666c65c79b2e104a534784b6c9bc8
choj <jack.cho@moesol.com>
2019-09-10 11:11:56 -0700

    Fixed action generators

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,6 @@
-export const searchChanged = (search) => { type: 'search_changed', payload: boolean }
-export const showDone = (show) => { type: 'show_done', payload: boolean }
-export const createChanged = (topic) => { type: 'create_changed', payload: boolean }
-export const createSubmited = (topic) => { type: 'create_submitted', payload: boolean }
-export const doneToggled = (idx) => { type: 'done_toggled', payload: boolean }
-export const  deleteTodo = (idx) => { type: 'delete_todo', payload: boolean }
+export const searchChanged = (search) => ({ type: 'search_changed', payload: search })
+export const showDone = (show) => ({ type: 'show_done', payload: show })
+export const createChanged = (topic) => ({ type: 'create_changed', payload: topic })
+export const createSubmited = (topic) => ({ type: 'create_submitted', payload: topic })
+export const doneToggled = (idx) => ({ type: 'done_toggled', payload: idx })
+export const  deleteTodo = (idx) => ({ type: 'delete_todo', payload: idx })
