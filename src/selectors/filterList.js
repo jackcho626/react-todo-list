1f95d4e894d59ce0ee115166ec4cfc3747c5a56e
choj <jack.cho@moesol.com>
2019-09-13 13:53:32 -0700

    Refactor

--- src/selectors/filterList.js
+++ src/selectors/filterList.js
@@ -1,6 +1,6 @@
 import * as R from 'ramda'
 import { createSelector } from 'reselect'
-import { selectFilters, selectHide, selectSearch } from '../reducers/filters'
+import { selectFilters } from '../reducers/filters'
 import { selectTodos } from '../reducers/todos'
 
 const doneLens = R.lensProp('done')
@@ -8,10 +8,15 @@ const doneLens = R.lensProp('done')
 export const selectFilteredList = createSelector(
     selectTodos,
     selectFilters,
-    (todos, filters) => {
-        const newTodos = todos.map((todo, idx) => R.assoc('idx', idx, todo))
-        const rej = R.reject(filters.hide ? R.view(doneLens, R.__) : R.F)(newTodos)
-        const filt = R.filter(todo => todo.task.toUpperCase().includes(selectSearch(filters).toUpperCase()))(rej)
-        return filt
-    }
+    // (todos, filters) => {
+    //     const newTodos = todos.map((todo, idx) => R.assoc('idx', idx, todo))
+    //     // const rej = R.reject(filters.hide ? R.view(doneLens, R.__) : R.F)(newTodos)
+    //     const rej = R.reject(filters.hide ? R.view(doneLens, R.__) : R.F)(newTodos)
+    //     const filt = R.filter(todo => todo.task.toUpperCase().includes(selectSearch(filters).toUpperCase()))(rej)
+    //     return filt
+    // }
+    (todos, filters) =>
+    todos.map((todo, idx) => ({ ...todo, idx }))
+        .filter(todo => filters.hide ? !todo.done: true)
+        .filter(todo => todo.task.toUpperCase().includes(filters.search.toUpperCase()))
 )
